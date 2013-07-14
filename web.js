b2b7a48bdbbb39218d298b333e38938298e9abea
Ziranov <mailziranov@gmail.com>
2013-07-14 23:44:22 +0000

    helloz

--- web.js
+++ web.js
@@ -2,8 +2,11 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs = require('fs');
+
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+
+  response.send('helloz');
 });
 
 var port = process.env.PORT || 5000;
